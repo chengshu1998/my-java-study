c3d07e23c5a1341f2d974b244bf3fa6eaf595221
Cheng557shu <1311446032@qq.com>
2021-11-30 11:39:26 +0800

    第二次提交

---
+++ module1/src/chengshu/test1/test1.java
@@ -0,0 +1,12 @@
+package chengshu.test1;
+
+/**
+ * @author chengshu1998
+ * @date 2021/11/30 - 11:38
+ **/
+public class test1 {
+    public static void main(String[] args) {
+        int[] a = new int[]{1,2,3};
+        System.out.println(a);
+    }
+}
