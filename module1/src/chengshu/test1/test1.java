6dcf5d0ed85866b2656c0d62beed69c133e8230b
Cheng557shu <1311446032@qq.com>
2021-12-01 12:52:07 +0800

    update

--- module1/src/chengshu/test1/test1.java
+++ module1/src/chengshu/test1/test1.java
@@ -6,7 +6,19 @@ package chengshu.test1;
  **/
 public class test1 {
     public static void main(String[] args) {
-        int[] a = new int[]{1,2,3};
-        System.out.println(a);
+        Base base = new Sub();
+        base.add(1, 2, 3);
+    }
+}
+
+class Base{
+    public void add(int a, int... arr){
+        System.out.println("base");
+    }
+}
+
+class Sub extends Base{
+    public void add(int a, int[] arr){
+        System.out.println("sub");
     }
 }
